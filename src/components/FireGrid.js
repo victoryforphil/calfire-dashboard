5e9df839f253a11a33d40f321fedaa84d2de5997
Alex Carter <victoryforphil@gmail.com>
2020-11-19 13:42:26 -0800

    Mac

--- src/components/FireGrid.js
+++ src/components/FireGrid.js
@@ -1,87 +1,133 @@
-import React from 'react';
-import { Button, List, Card, Spin } from 'antd';
+import React, { useState, useEffect } from "react";
+import { Button, List, Card, Spin } from "antd";
 
-import FireItem from "./FireItem"
+import FireItem from "./FireItem";
 
-export default class FireGrid extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            items: [],
-            location: null
-        };
+export default function FireGrid(props) {
+    let [items, setItems] = useState([]);
+    let [isLoaded, setLoaded] = useState(false);
+    let [error, setError] = useState(null);
+
+    let { sort } = props;
+
+
+    function loadData() {
+        fetch(
+            "https://cors-anywhere.herokuapp.com/https://www.fire.ca.gov/umbraco/api/IncidentApi/List?inactive=false&year=2020"
+        )
+            .then((res) => res.json())
+            .then(
+                (result) => {
+
+                    setLoaded(true);
+                    setItems(result);
+
+                    sortResults();
+                },
+                (error) => {
+                    setLoaded(true);
+                    setError(error);
+                }
+            );
     }
-    getLocation() {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition((pos)=>{this.sortByLocation(pos)});
-        } else {
-          
-        }
-      }
+    function sortResults() {
+        switch(sort.key){
+            case "LOCATION":
+                getLocation();
+            break;
+            case "UPDATED":
+                sortUpdated();
+                break;
+            case "NEWEST":
+                sortCreate(false);
+                break;
+            case "OLDEST":
+                sortCreate(true);
+                break;
+            case "LARGEST":
+                sortSize();
+                break;
+            default:
+                getLocation();
 
-    sortByLocation(pos){
+        }
+    }
+
+    function sortCreate(sortOld){
+
+    }
+
+    function sortUpdated(){
+
+    }
+
+    function sortSize(){
+
+    }
+
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((pos) => {
+                sortByLocation(pos);
+            });
+        } else {
+        }
+    }
+
+    function sortByLocation(pos) {
         const long = pos.coords.longitude;
         const lat = pos.coords.latitude;
 
-        const oldItems = this.state.items;
-        this.setState({items: []})
-        const newTiems = oldItems.sort(function(a, b) {
+        const oldItems = items;
+        setItems([])
+        const newTiems = oldItems.sort(function (a, b) {
             //this.props.fire.Latitude, lng: this.props.fire.Longitude
             const errorA = Math.abs(a.Latitude - lat) + Math.abs(a.Longitude - long);
             const errorB = Math.abs(b.Latitude - lat) + Math.abs(b.Longitude - long);
-            if(errorA > errorB){return 1}
-            if(errorA < errorB){return -1}
-            if(errorA === errorB){return 0}
-          });
-        this.setState({items: newTiems, isloaded: true})
-    }
-    componentDidMount() {
-        fetch("https://cors-anywhere.herokuapp.com/https://www.fire.ca.gov/umbraco/api/IncidentApi/List?inactive=false&year=2020")
-            .then(res => res.json())
-            .then(
+            if (errorA > errorB) {
+                return 1;
+            }
+            if (errorA < errorB) {
+                return -1;
+            }
+            if (errorA === errorB) {
+                return 0;
+            }
+        });
+        setItems(newTiems);
+        setLoaded(true);
 
-                (result) => {
-                    console.log(result);
-                    this.setState({
-                        isLoaded: true,
-                        items: result
-                    });
-
-                    this.getLocation();
-                },
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                }
-            )
     }
-    render() {
-        if(this.state.isLoaded === false || this.state.items.length < 1){
-            return (<Spin></Spin>)
+
+    useEffect(() => {
+        if (!isLoaded) {
+            loadData();
         }
-        
-        return (
-            <List
-                grid={{
-                    gutter: 16,
-                    xs: 1,
-                    sm: 2,
-                    md: 4,
-                    lg: 4,
-                    xl: 4,
-                    xxl: 4,
-                }}
-                dataSource={this.state.items}
-                renderItem={item => (
-                    <List.Item>
-                        <FireItem fire={item}></FireItem>
-                    </List.Item>
-                )}
-            />
-        )
+    })
+    if (isLoaded === false || items.length < 1) {
+        return <Spin></Spin>;
     }
+
+    return (
+        <List
+            grid={{
+                gutter: 16,
+                xs: 1,
+                sm: 2,
+                md: 4,
+                lg: 4,
+                xl: 4,
+                xxl: 4,
+            }}
+            dataSource={items}
+            renderItem={(item) => (
+                <List.Item>
+                    <FireItem fire={item}></FireItem>
+                </List.Item>
+            )}
+        />
+    );
 }
+
+
--- src/pages/PageMain.js
+++ src/pages/PageMain.js
@@ -1,30 +1,67 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
   Redirect,
-  useParams
+  useParams,
 } from "react-router-dom";
-import { Button, PageHeader } from 'antd';
-import FireDetail from "../components/FireDetail"
+import { DownOutlined, UserOutlined } from '@ant-design/icons';
+
+import { Button, PageHeader , Dropdown, Menu} from "antd";
+import FireDetail from "../components/FireDetail";
 import FireGrid from "../components/FireGrid";
 
-
 export default function PageMain() {
-    // We can use the `useParams` hook here to access
-    // the dynamic pieces of the URL.
+  // We can use the `useParams` hook here to access
+  // the dynamic pieces of the URL.
+  const filters = [
+    {key: "LOCATION", display: "Closest (Requires Location)"},
+    {key: "UPDATED", display: "Recently Updated"},
+    {key: "NEWEST", display: "Newest"},
+    {key: "OLDEST", display: "Oldest"},
+    {key: "LARGEST", display: "Largest"}
+  ]
+  let [filter, setFilter] = useState(filters[0])
+  function onFilterClick(clickItem){
     
-
-    return (
-        <div>
-          <PageHeader
-    className="site-page-header"
-    title="CALFire Watch"
-    subTitle={"All Fires (sorted by location)"}
-  />
-      <FireGrid/>
-        </div>
-    );
+    filters.forEach((cur)=>{
+      if(cur.key === clickItem.key){
+        console.log(cur);
+        setFilter(cur);
+      }
+    })
   }
+  function generateMenu(){
+    return(
+      <Menu onClick={onFilterClick}>
+        {
+          filters.map(cur=>
+            (
+              <Menu.Item key={cur.key}>
+              {cur.display}
+              </Menu.Item>
+            ))
+        }
+      </Menu>
+    )
+  }
+  return (
+    <div>
+      <PageHeader
+        className="site-page-header"
+        title="CALFire Watch"
+        subTitle={`All Fires (sorted by ${filter.display})`}
+        footer={
+          <Dropdown overlay={generateMenu()}>
+      <Button>
+        Sort By <DownOutlined />
+      </Button>
+    </Dropdown>
+        }
+      />
+      <FireGrid sort={filter}/>
+    </div>
+  );
+}
